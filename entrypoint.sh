0799da9d6149913b30a0396918a682a6bf554962
我怎么就不是一只猫呢? <1179551960@qq.com>
2022-03-24 16:38:47 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ then
 	htpasswd -bc /etc/nginx/htpasswd $USERNAME $PASSWORD
 	echo Done.
 else
-    echo Using no auth.
+	echo Using no auth.
 	sed -i 's%auth_basic "Restricted";% %g' /etc/nginx/conf.d/default.conf
 	sed -i 's%auth_basic_user_file /etc/nginx/htpasswd;% %g' /etc/nginx/conf.d/default.conf
 fi
